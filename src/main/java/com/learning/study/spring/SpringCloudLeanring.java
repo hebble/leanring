7b81d2b0aec39240dd8f5ed593520900d3178ea1
zhanghongbo <zhebble@163.com>
2022-09-19 17:26:00 +0800

    u

--- src/main/java/com/learning/study/spring/SpringCloudLeanring.java
+++ src/main/java/com/learning/study/spring/SpringCloudLeanring.java
@@ -35,6 +35,23 @@ public class SpringCloudLeanring {
                  3.系统集成测试比较麻烦
                  4.性能的监控比较麻烦。【最好开发一个大屏监控系统】
             总的来说优点大过于缺点，目前看来Spring Cloud是一套非常完善的分布式框架，目前很多企业开始用微服务、Spring Cloud的优势是显而易见的。因此对于想研究微服务架构的同学来说，学习Spring Cloud是一个不错的选择。
+        1.5 SpringBoot和SpringCloud的区别？
+             SpringBoot专注于快速方便的开发单个个体微服务。
+             SpringCloud是关注全局的微服务协调整理治理框架，它将SpringBoot开发的一个个单体微服务整合并管理起来，
+             为各个微服务之间提供，配置管理、服务发现、断路器、路由、微代理、事件总线、全局锁、决策竞选、分布式会话等等集成服务
+             SpringBoot可以离开SpringCloud独立使用开发项目， 但是SpringCloud离不开SpringBoot ，属于依赖的关系
+             SpringBoot专注于快速、方便的开发单个微服务个体，SpringCloud关注全局的服务治理框架。
+        1.6 SpringCloud由什么组成
+             这就有很多了，我讲几个开发中最重要的
+             Spring Cloud Eureka：服务注册与发现
+             Spring Cloud Zuul：服务网关
+             Spring Cloud Ribbon：客户端负载均衡
+             Spring Cloud Feign：声明性的Web服务客户端
+             Spring Cloud Hystrix：断路器
+             Spring Cloud Confifig：分布式统一配置管理
+             等20几个框架，开源一直在更新
+        1.7 Spring Cloud 和dubbo区别?
+            
 
      */
 }
