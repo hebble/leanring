018ce3be6068fc7c64f4eca3341692a00d8d74c4
zhanghongbo <zhebble@163.com>
2022-10-15 17:52:06 +0800

    u

--- src/main/java/com/learning/study/third/JvmLearning.java
+++ src/main/java/com/learning/study/third/JvmLearning.java
@@ -1,8 +1,10 @@
 package com.learning.study.third;
 
 /**
- * https://blog.csdn.net/banzhuanhu/article/details/106871495
- * https://www.cnblogs.com/xfeiyun/p/15835733.html
+ * https://blog.csdn.net/banzhuanhu/article/details/106871495 2020面试阿里字节跳动90%被问到的JVM面试题附答案
+ * https://www.cnblogs.com/xfeiyun/p/15835733.html 四种引用类型: 强引用,软引用, 弱引用, 虚引用
+ * https://blog.csdn.net/Lee_92/article/details/126170594 JVM相关工具的使用——JvisualVM
+ * https://blog.csdn.net/qq_31960623/article/details/120541633 常见的几款JVM监控工具
  */
 public class JvmLearning {
     /**
@@ -204,5 +206,87 @@ public class JvmLearning {
          (4)虚引用：虚引用就是一种可有可无的引用，无法用来表示对象的生命周期，任何时候都可能被回收，虚引用主要使用来跟踪对象被垃圾回收的活动，虚引用和软引用与弱引用的区别在于：虚引用必须和引用队
             列联合使用；在进行垃圾回收的时候，如果发现一个对象只有虚引用，那么就会将这个对象的引用加入到与之关联的引用队列中，程序可以通过发现一个引用队列中是否已经加入了虚引用，来了解被引用的对
             象是否需要被进行垃圾回收；
+
+     15.项目中怎么优化JVM的?
+        一个服务的Java参数配置：（服务器：Linux 64Bit，16Core×32G）
+            JAVA_OPTS=“$JAVA_OPTS -server -Xmx20G -Xms20G -XX:NewRatio=3 -XX:SurvivorRatio=4 -Xss256k -XX:MaxTenuringThreshold=15 -XX:+AggressiveOpts -XX:+UseBiasedLocking -XX:+UseFastAccessorMethods
+            -XX:+DisableExplicitGC -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:CMSFullGCsBeforeCompaction=1 -XX:+UseCMSCompactAtFullCollection
+            -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -Xloggc:logs/gc-%t.log
+            -XX:+HeapDumpOnOutOfMemoryError”
+
+     16.jvisualvm监控远程jvm
+         要使用jmx远程监控功能，在远程java application启动时，必须通过jvm选项配置提供远程服务的端口：
+            -Dcom.sun.management.jmxremote.port=9015
+         一般同时指定为本地jmx client提供服务的端口与远程服务端口相同：
+            -Dcom.sun.management.jmxremote.rmi.port=9015
+         jmx远程服务默认是开启ssl和认证功能功能的，也可以通过jvm选项把这两个功能关闭：
+            -Dcom.sun.management.jmxremote.authenticate=false
+            -Dcom.sun.management.jmxremote.ssl=false
+         还可以设置虚拟机参数：
+            -Xms256m -Xmx512m -Xss256m -XX:PermSize=512m -XX:MaxPermSize=1024m
+         jmx默认是通过localhost的ip地址提供RMI服务的，如果要明确指定RMI服务地址或主机名（比如主机有多个接口，想使用非hostname关联的接口），可以通过以下选项显式指定：
+            -Djava.rmi.server.hostname=服务器IP
+
+        这样开启jmx远程监控功能的最小配置具体如下：
+             -Xms256m
+             -Xmx512m
+             -Xss256m
+             -XX:PermSize=512m
+             -XX:MaxPermSize=1024m
+             -Dcom.sun.management.jmxremote
+             -Djava.rmi.server.hostname=服务器IP
+             -Dcom.sun.management.jmxremote.port=9015
+             -Dcom.sun.management.jmxremote.rmi.port=9015
+             -Dcom.sun.management.jmxremote.ssl=false
+             -Dcom.sun.management.jmxremote.authenticate=false
+
+     17.常见的几款JVM监控工具
+        在生产环境中，经常会遇到各种各样奇葩的性能问题，所以掌握最基本的JVM命令行监控工具还是很有必要的
+             名称	主要作用
+             jps	查看正在运行的Java进程
+             jstack	打印线程快照
+             jmap	导出堆内存映像文件
+             jstat	查看jvm统计信息
+             jinfo	实时查看和修改jvm配置参数
+             jhat	用于分析heapdump文件
+        17.1 jps
+            jps可以列出正在运行的Java进程，并显示虚拟机执行主类（Main Class，main()函数所在的类）名称以及进程id
+                 选项	作用
+                 -q	只输出进程id
+                 -m	输出传递给主类main函数的参数
+                 -l	输出主类全类名，如果进程执行的是Jar包，输出jar包名字
+                 -v	程序启动时指定的jvm参数
+        17.2 jstack
+            jstack:打印线程快照, 查看某个Java进程中所有线程的状态。一般用来定位线程出现长时间停顿的原因，如发生死循环，死锁，请求外部资源长时间等待等！
+            jstack pid
+            图忽略
+            从图中可以看到有很多线程，GC线程，myThreadA，myThreadB等，其中myThreadA和myThreadB的线程状态为BLOCKED，并且在最后提示出了死锁发生的位置
+        17.3 jmap
+            jmap:导出堆内存映像文件, jmap主要用来用来导出堆内存映像文件，看是否发生内存泄露等。
+            生产环境我们一般会配置如下参数，让虚拟机在OOM异常出现之后自动生成dump文件
+                 -XX:+HeapDumpOnOutOfMemoryError:在程序发生oom的时候,导出应用程序的当前dump文件.
+                 -XX:HeapDumpPath=<filename.hprof> 指定堆快照的保存位置..
+            执行如下命令即可手动获得dump文件
+                jmap -dump:file=文件名.dump 进程id
+        17.4 jstat
+            jstat:查看jvm统计信息, jstat可以显示本地或者远程虚拟机进程中的类装载、 内存、 垃圾收集、 JIT编译等运行数据
+            jstat查看一下类装载的信息。我个人很少使用这个命令，命令行看垃圾收集信息真不如看图形界面方便，就不多做介绍了。
+                 [root@VM-0-14-centos ~]# jstat -class 19402
+                 Loaded  Bytes  Unloaded  Bytes     Time
+                 10229 19679.1       52    76.0       5.33
+             选项	作用
+             Loaded	加载类的个数
+             Bytes	加载类的字节数
+             Unloaded	卸载类的个数
+             Bytes	卸载类的字节数
+             Time	花费的时间
+        17.5 jinfo
+            jinfo:实时查看和修改jvm配置参数, jinfo的作用是实时地查看和修改虚拟机各项参数。
+            「使用jps命令的-v参数可以查看虚拟机启动时显式指定的参数列表」，但如果想知道未被显式指定的参数的系统默认值，除了去找资料外，就只能使用jinfo的-flag选项进行查询了（如果只限于JDK 1.6或以上版本的话，使用java -XX:+PrintFlagsFinal查看参数默认值也是一个很好的选择
+             jinfo -flags pid (jinfo flags pid在高版本才能正常使用，我在jdk1.8这个版本不能正常使用)
+             jinfo -flag MaxHeapSize pid #查看最大堆内存
+             jinfo -flag PrintGCDetails pid #查看是否打印GC日志
+
+
      */
 }
