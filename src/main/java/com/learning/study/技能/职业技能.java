13f93ba28f9b32b7867090d51ef636f26d43297d
zhanghongbo <zhebble@163.com>
2023-02-08 15:34:50 +0800

    增加职业技能

---
+++ src/main/java/com/learning/study/技能/职业技能.java
@@ -0,0 +1,15 @@
+package com.learning.study.技能;
+
+public class 职业技能 {
+    /**
+     1.扎实 JAVA 基础知识,具有面向对象,面向切面编程思想,对集合,IO ,多线程,JDK 锁等有深入理解
+     2.熟悉 JVM 内存模型,GC 算法,垃圾回收器,类加载机制,具备一定的 JVM 调优经验
+     3.熟练 Spring, Mybatis, SpringBoot, SpringCloud 等主流框架,熟悉 IOC,AOP ,自动装配原理
+     4.熟练 Nacos, Gateway, OpenFeign, Sentinel 等微服务架构组件, 拥有丰富的微服务实战经验
+     5.熟悉分布式系统的设计和应用,熟悉 CAP,BASE 理论,拥有分布式锁,分布式事务,分布式定时任务,分布式链路追踪的深入理解
+     6.熟练使用常用消息中间件 Kafka, RabbitMq ,掌握相关系统架构设计,有丰富的实战和性能优化经验,
+     7.熟练使用搜索中间件 ElasticSearch ,熟悉倒排索引,评分原理, 掌握 es 性能优化
+     8.熟练使用 Mysql ,掌握索引底层原理,事务和锁机制,底层日志,熟悉集群主从复制原理,掌握 sql 优化并具备线上项目调优经验
+     9.熟练使用 Redis 做缓存,分布式锁,布隆过滤器,熟悉持久化,淘汰机制,高可用部署模式
+     */
+}
