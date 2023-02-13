eb1c2d82bec45be5eac18da7da0f841de92b0bca
zhanghongbo <zhebble@163.com>
2023-02-13 17:22:28 +0800

    u

--- src/main/java/com/learning/study/分布式/分布式事务.java
+++ src/main/java/com/learning/study/分布式/分布式事务.java
@@ -57,6 +57,12 @@ public class 分布式事务 {
     8.Seata
         8.1 Seata介绍
             Seata 是一款开源的分布式事务解决方案，致力于提供高性能和简单易用的分布式事务服务。Seata 将为用户提供了 AT、TCC、SAGA 和 XA 事务模式，为用户打造一站式的分布式解决方案。
+             TC (Transaction Coordinator) - 事务协调者
+                维护全局和分支事务的状态，驱动全局事务提交或回滚。
+             TM (Transaction Manager) - 事务管理器
+                定义全局事务的范围：开始全局事务、提交或回滚全局事务。
+             RM (Resource Manager) - 资源管理器
+                管理分支事务处理的资源，与TC交谈以注册分支事务和报告分支事务的状态，并驱动分支事务提交或回滚。
         8.2 Seata AT
             Seata 的 AT 模式（Automatic Transaction）是一种无侵入的分布式事务解决方案。
 
